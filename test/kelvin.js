0f2f4148fd2aa8acaf52a9a2dd21130fd5fd772c
Jesus David <20200750@uthh.edu.mx>
2023-10-27 18:57:01 -0600

    update

--- test/kelvin.js
+++ test/kelvin.js
@@ -6,7 +6,7 @@ fixture`Prueba de Conversión de Temperatura`
 test('Prueba de conversión de temperatura a Kelvin', async t => {
     const input = Selector('#celsius');
     await t
-        .typeNumber(input, '25')
+        .typeNumber("#celsius", '25')
         .expect(input.value).eql('25');
 
     const message = Selector('#mensaje').innerText;
